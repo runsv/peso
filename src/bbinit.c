31b6fa0780c682783ad9963eef5ba6d3d0edbb2f
Babis <babis@isa.ti.gr>
2019-04-06 05:06:44 +0200

    split main() function into subroutines

--- src/bbinit.c
+++ src/bbinit.c
@@ -17,6 +17,7 @@
 #include <sys/stat.h>
 #include <sys/time.h>
 #include <sys/resource.h>
+#include <libgen.h>
 #include <paths.h>
 
 /* path to the BusyBox binary
@@ -60,6 +61,35 @@ static void setup_env ( void )
   Env [ 4 ] = ( s && * s ) ? s - 8 : "CONSOLE=" CONSOLE ;
 }
 
+/* execve(2) into a given executable */
+static void exec_into ( char * const x, char ** av )
+{
+  char * b = (char *) NULL ;
+
+  (void) fflush ( NULL ) ;
+  /* not too helpful when using our default PATH :-\ */
+  (void) execve ( x, av, Env ) ;
+  perror ( "execve(2) failed" ) ;
+  (void) fflush ( NULL ) ;
+  b = basename ( x ) ;
+
+  if ( b && ( x != b ) && * b ) {
+#if defined (__GLIBC__) && defined (_GNU_SOURCE)
+    (void) execvpe ( b, av, Env ) ;
+#endif
+    (void) execvp ( b, av ) ;
+    perror ( "execve(2) failed" ) ;
+    (void) fflush ( NULL ) ;
+  }
+
+#if defined (__GLIBC__) && defined (_GNU_SOURCE)
+  (void) execvpe ( * av, av, Env ) ;
+#endif
+  (void) execvp ( * av, av ) ;
+  perror ( "execve(2) failed" ) ;
+  (void) fflush ( NULL ) ;
+}
+
 static int setup_rlimits ( void )
 {
   struct rlimit rlim ;
@@ -69,69 +99,101 @@ static int setup_rlimits ( void )
   return setrlimit ( RLIMIT_CORE, & rlim ) ;
 }
 
+/* execve(2) into the real init binary */
+static void run_init ( const int argc, char ** argv )
+{
+  if ( 2 > argc ) {
+    char * av [ 2 ] = { "init", (char *) NULL } ;
+    exec_into ( BB, av ) ;
+    exec_into ( "/bin/busybox", av ) ;
+    exec_into ( "/bin/toybox", av ) ;
+    exec_into ( "/sbin/sysv-init", av ) ;
+  } else {
+    argv [ 0 ] = "init" ;
+    exec_into ( BB, argv ) ;
+    exec_into ( "/bin/busybox", argv ) ;
+    exec_into ( "/bin/toybox", argv ) ;
+    exec_into ( "/sbin/sysv-init", argv ) ;
+  }
+}
+
+static void sulogin ( void )
+{
+  char * av [ 2 ] = { "sulogin", (char *) NULL } ;
+  exec_into ( "/sbin/sulogin", av ) ;
+}
+
+static void sh ( void )
+{
+  char * av [ 3 ] = { (char *) NULL } ;
+
+  av [ 1 ] = "-i" ;
+  av [ 0 ] = "zsh" ;
+  exec_into ( "/usr/bin/zsh", av ) ;
+  av [ 0 ] = "bash" ;
+  /*
+  exec_into ( "/usr/bin/bash", av ) ;
+  */
+  exec_into ( "/bin/bash", av ) ;
+  av [ 0 ] = "ksh" ;
+  exec_into ( "/bin/mksh", av ) ;
+  exec_into ( "/bin/lksh", av ) ;
+  exec_into ( "/bin/ksh93", av ) ;
+  exec_into ( "/bin/ksh", av ) ;
+  av [ 0 ] = "tcsh" ;
+  exec_into ( "/usr/bin/tcsh", av ) ;
+  av [ 0 ] = "sh" ;
+  exec_into ( "/bin/ash", av ) ;
+  exec_into ( "/bin/dash", av ) ;
+  av [ 1 ] = (char *) NULL ;
+  exec_into ( "/bin/sh", av ) ;
+}
+
+static int rescue ( void )
+{
+  (void) fprintf ( stderr,
+    "\ninit: Could not find the \"busybox\" binary in PATH\n\t%s\ninit: Execing into sulogin ...\n\n"
+    , PATH ) ;
+  (void) fflush ( NULL ) ;
+  sulogin () ;
+  (void) fprintf ( stderr,
+    "\ninit: Could not find the \"sulogin\" utility.\ninit: Execing into the \"%s\" shell ...\n\n"
+    , SHELL ) ;
+  sh () ;
+  (void) fflush ( NULL ) ;
+  /* very deep shit: kernel panic ahead */
+  return 111 ;
+}
+
 int main ( const int argc, char ** argv )
 {
   if ( 1 < getpid () ) {
     (void) fputs ( "\ninit: Must run as process #1.\n\n", stderr ) ;
+    (void) fflush ( NULL ) ;
     return 100 ;
   }
 
   if ( getuid () || geteuid () ) {
     (void) fputs ( "\ninit: Must be super-user.\n\n", stderr ) ;
+    (void) fflush ( NULL ) ;
     return 100 ;
   }
 
-  if ( 0 < argc ) {
-    argv [ 0 ] = "init" ;
-  }
-
-  /* set up the process environment */
+  /* sane process environment */
   setup_env () ;
   (void) umask ( 00022 ) ;
   (void) setup_rlimits () ;
   (void) setsid () ;
   (void) setpgid ( 0, 0 ) ;
   (void) chdir ( "/" ) ;
-  /* set a default hostname that can be changed later */
+
+  /* set a preliminary default hostname that can be easily changed later */
+#ifdef HOSTNAME
   (void) sethostname ( HOSTNAME, strlen ( HOSTNAME ) ) ;
-
-  /* execve(2) into the real BusyBox binary */
-  (void) execve ( BB, argv, Env ) ;
-  (void) execve ( "/bin/busybox", argv, Env ) ;
-  (void) execve ( "/sbin/busybox", argv, Env ) ;
-  (void) execve ( "/usr/bin/busybox", argv, Env ) ;
-  (void) execve ( "/usr/sbin/busybox", argv, Env ) ;
-  /* not too helpful when using our default PATH :-\ */
-#if defined (__GLIBC__) && defined (_GNU_SOURCE)
-  (void) execvpe ( "busybox", argv, Env ) ;
 #endif
-  (void) execvp ( "busybox", argv ) ;
-  perror ( "execve(2) failed" ) ;
 
-  /* BusyBox binary was not found, we are hosed now.
-   * do something to prevent kernel panicking:
-   * exec into shell or sulogin ?
-   */
-  (void) fprintf ( stderr,
-    "\ninit: Could not find the \"busybox\" binary in PATH\n\t%s\ninit: Execing into sulogin ...\n\n"
-    , PATH ) ;
-  (void) fflush ( NULL ) ;
-  (void) execle ( "/sbin/sulogin", "sulogin", (char *) NULL, Env ) ;
-  (void) execle ( "/bin/sulogin", "sulogin", (char *) NULL, Env ) ;
-  perror ( "execve(2) failed" ) ;
-  (void) fprintf ( stderr,
-    "\ninit: Could not find the \"sulogin\" utility.\ninit: Execing into the \"%s\" shell ...\n\n"
-    , SHELL ) ;
-  (void) fflush ( NULL ) ;
-  (void) execle ( SHELL, SHELL, (char *) NULL, Env ) ;
-  (void) execlp ( "bash", "bash", "-i", (char *) NULL ) ;
-  (void) execlp ( "mksh", "mksh", "-i", (char *) NULL ) ;
-  (void) execlp ( "ksh", "ksh", "-i", (char *) NULL ) ;
-  (void) execle ( "/bin/sh", "sh", (char *) NULL, Env ) ;
-  (void) execlp ( "sh", "sh", (char *) NULL ) ;
-  perror ( "execve(2) failed" ) ;
+  run_init ( argc, argv ) ;
 
-  /* very deep shit: kernel panic ahead */
-  return 111 ;
+  return rescue () ;
 }
 
